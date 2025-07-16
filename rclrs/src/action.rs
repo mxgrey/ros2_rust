ab0e03537829a29ca857bd7777ea1edecb571a80
Michael X. Grey <greyxmike@gmail.com>
2025-07-16 18:50:19 +0800

    Some more iterating on actions API
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- rclrs/src/action.rs
+++ rclrs/src/action.rs
@@ -9,6 +9,7 @@ use futures::{
 use std::{
     future::Future,
     sync::Arc,
+    pin::pin,
 };
 
 use tokio::sync::watch;
@@ -31,10 +32,10 @@ impl NodeState {
     pub fn create_action<A: Action, F>(
         &self,
         options: ActionOptions,
-        callback: impl FnMut(ActionGoalHandle<A>) -> F,
+        callback: impl FnMut(GoalHandle<A>) -> F,
     )
     where
-        F: Future<Output = ActionGoalDecision<A>>,
+        F: Future<Output = GoalDecision<A>>,
     {
 
     }
@@ -42,38 +43,30 @@ impl NodeState {
 
 
 
-pub enum ActionGoalDecision<A: Action> {
-    Accepted(ActionExecution<A>),
-    Rejected,
+pub enum GoalDecision<A: Action> {
+    Accept(ActionExecution<A>),
+    Reject,
 }
 
-pub struct ActionGoalHandle<A: Action> {
+pub struct GoalHandle<A: Action> {
     goal: Arc<A::Goal>,
 }
 
-impl<A: Action> ActionGoalHandle<A> {
-    pub fn accept<F>(
+impl<A: Action> GoalHandle<A> {
+    pub fn accept_and_execute<F>(
         self,
-        execute: impl FnOnce(ActionExecutionHandle<A>) -> F,
-    ) -> ActionGoalDecision<A>
+        execute: impl FnOnce(ActionHandle<A>) -> F,
+    ) -> GoalDecision<A>
     where
         F: Future<Output = ActionResult<A>>
     {
 
     }
-}
 
-pub struct ActionAcceptedQueue<A: Action> {
-    _ignore: std::marker::PhantomData<fn(A)>,
-}
-
-impl<A: Action> ActionAcceptedQueue<A> {
-    /// Accept the action, but put it in a queue
-    pub fn accept<F>(
-        self: Arc<Self>,
-        handle: ActionGoalHandle<A>,
-        f: impl FnOnce(oneshot::Receiver<ActionQueueResult<A>>) -> F,
-    ) -> ActionGoalDecision<A>
+    pub fn accept_and_defer(
+        self,
+        defer: impl FnOnce(AcceptedAction<A>) -> F,
+    ) -> GoalDecision<A>
     where
         F: Future<Output = ActionExecution<A>>,
     {
@@ -81,25 +74,108 @@ impl<A: Action> ActionAcceptedQueue<A> {
     }
 }
 
-pub struct ActionExecutionHandle<A: Action> {
-    goal: Arc<A::Goal>,
-    feedback: Sender<A::Feedback>,
-    canceled: watch::Receiver<bool>,
+pub struct ActionQueue<A: Action> {
+    _ignore: std::marker::PhantomData<fn(A)>,
 }
 
-impl<A: Action> ActionExecutionHandle<A> {
-    pub async fn recv_cancel(&self) -> () {
-        self.canceled.clone().wait_for(|canceled| *canceled);
+impl<A: Action> ActionQueue<A> {
+    /// Accept the action, but put it in a queue
+    pub fn accept<F>(
+        self: Arc<Self>,
+        handle: GoalHandle<A>,
+        f: impl FnOnce(ActionQueueHandle<A>) -> F,
+    ) -> GoalDecision<A>
+    where
+        F: Future<Output = ActionExecution<A>>,
+    {
+
+    }
+}
+
+pub struct ActionQueueHandle<A: Action> {
+    ready: oneshot::Receiver<ActionQueueResult<A>>,
+}
+
+pub type UntilReadyResult<A, F> = Result<
+    (<F as Future>::Output, ActionQueueHandle<A>),
+    (ActionQueueResult<A>, F)
+>;
+
+impl<A: Action> ActionQueueHandle<A> {
+    pub async fn ready(self) -> ActionQueueResult<A> {
+        self.ready.await
+        .unwrap_or_else(|_| ActionQueueResult::Cancel(CancelingAction { _ignore: Default::default() }))
     }
 
-    pub async fn until_canceled<F: Future>(&self, f: F) -> Result<F::Output, F> {
-        let canceled = self.canceled.clone().wait_for(|canceled| *canceled);
-        pin_mut!(f);
-        pin_mut!(canceled);
-        match futures::future::select(f, canceled).await {
+    pub fn until_ready<F>(self, f: F) -> UntilReadyResult<A, F>
+    where
+        F: Future,
+    {
+
+    }
+
+    pub fn abort(self) -> ActionExecution<A> {
+
+    }
+}
+
+pub struct ActionHandle<A: Action> {
+    goal: Arc<A::Goal>,
+    feedback: Sender<A::Feedback>,
+    cancellation: CancellationWatcher<A>,
+}
+
+pub type UntilCancelledResult<A, F> = Result<
+    <F as Future>::Output,
+    (Cancellation<A>, F),
+>;
+
+impl<A: Action> ActionHandle<A> {
+    pub async fn recv_cancel(&self) -> Cancellation<A> {
+        self.cancellation.recv_cancel().await
+    }
+
+    pub async fn until_cancelled<F: Future + Unpin>(&self, f: F) -> UntilCancelledResult<A, F> {
+        self.cancellation.until_cancelled(f).await
+    }
+}
+
+#[derive(Clone)]
+pub struct CancellationWatcher<A: Action> {
+    watcher: watch::Receiver<bool>,
+    _ignore: std::marker::PhantomData<A>,
+}
+
+impl<A: Action> CancellationWatcher<A> {
+    pub async fn recv_cancel(&self) -> Cancellation<A> {
+        self.watcher.clone().wait_for(|canceled| *canceled);
+        Cancellation { _ignore: Default::default() }
+    }
+
+    pub async fn until_cancelled<F: Future + Unpin>(
+        &self,
+        f: F,
+    ) -> UntilCancelledResult<A, F> {
+        let mut watcher = self.watcher.clone();
+        let cancelled = watcher.wait_for(|canceled| *canceled);
+        pin_mut!(cancelled);
+        match futures::future::select(f, cancelled).await {
             futures::future::Either::Left((result, _)) => Ok(result),
-            futures::future::Either::Right((_, f)) => Err(f.into_inner()),
+            futures::future::Either::Right((_, f)) => Err((
+                Cancellation { _ignore: Default::default() },
+                f
+            )),
         }
+    }
+}
+
+pub struct Cancellation<A> {
+    _ignore: std::marker::PhantomData<A>,
+}
+
+impl<A: Action> Cancellation<A> {
+    pub fn canceled(result: A::Result) -> ActionResult<A> {
+
     }
 }
 
@@ -108,28 +184,55 @@ pub struct ActionExecution<A: Action> {
 }
 
 pub enum ActionQueueResult<A: Action> {
-    Execute(ExecuteAction<A>),
-    Cancel(ExecuteAction<A>)
+    Execute(AcceptedAction<A>),
+    Cancel(CancelingAction<A>),
 }
 
-pub enum ActionResult<A: Action> {
+pub struct ActionResult<A: Action> {
+    inner: InnerActionResult<A>,
+}
+
+enum InnerActionResult<A: Action> {
     Succeeded(A::Result),
     Canceled(A::Result),
     Aborted,
 }
 
-pub struct ExecuteAction<A: Action> {
+pub struct AcceptedAction<A: Action> {
     _ignore: std::marker::PhantomData<A>,
 }
 
-impl<A: Action> ExecuteAction<A> {
-    pub fn begin<F>(
+impl<A: Action> AcceptedAction<A> {
+    pub fn execute<F>(
         self,
-        f: impl FnOnce(ActionExecutionHandle<A>) -> F,
+        f: impl FnOnce(ActionHandle<A>) -> F,
     ) -> ActionExecution<A>
     where
         F: Future<Output = ActionResult<A>>,
     {
+
+    }
+}
+
+pub struct CancelingAction<A: Action> {
+    _ignore: std::marker::PhantomData<fn(A)>,
+}
+
+impl<A: Action> CancelingAction<A> {
+    /// Enter the Canceling state and do whatever work is needed to respond to
+    /// the cancellation request. This still allows you to respond with Succeed
+    /// or Abort instead of Canceled, but the recommended final transition is
+    /// Canceled using [`Cancellation::canceled`].
+    pub fn begin_canceling<F>(
+        self,
+        f: impl FnOnce(ActionHandle<A>, Cancellation<A>),
+    ) -> ActionExecution<A> {
+
+    }
+
+    /// Immediately report the action as canceled. This can be used if no work
+    /// needs to be done to cancel the action.
+    pub fn canceled(self, result: A::Result) -> ActionExecution<A> {
 
     }
 }
@@ -145,7 +248,7 @@ mod tests {
         }
     };
 
-    use example_interfaces::action::{Fibonacci, Fibonacci_Result};
+    use example_interfaces::action::{Fibonacci, Fibonacci_Result, Fibonacci_Feedback};
 
     fn test_create_action(node: &NodeState) {
         node.create_action(
@@ -155,13 +258,13 @@ mod tests {
     }
 
     async fn accept_fibonacci_action(
-        handle: ActionGoalHandle<Fibonacci>
-    ) -> ActionGoalDecision<Fibonacci> {
-        handle.accept(run_fibonacci_action)
+        handle: GoalHandle<Fibonacci>
+    ) -> GoalDecision<Fibonacci> {
+        handle.accept_and_execute(run_fibonacci_action)
     }
 
     async fn run_fibonacci_action(
-        handle: ActionExecutionHandle<Fibonacci>,
+        handle: ActionHandle<Fibonacci>,
     ) -> ActionResult<Fibonacci> {
         let order = handle.goal.order;
         let mut sequence = Vec::new();
@@ -181,9 +284,10 @@ mod tests {
         });
 
         loop {
-            match handle.until_canceled(receiver.next()).await {
+            match handle.until_cancelled(receiver.next()).await {
                 Ok(Some(next)) => {
                     sequence.push(next);
+
                 }
                 Ok(None) => {
                     // The sequence has finished
@@ -197,11 +301,11 @@ mod tests {
     }
 
     async fn wait_to_run_fibonacci(
-        ready: oneshot::Receiver<ActionQueueResult<Fibonacci>>,
+        handle: ActionQueueHandle<Fibonacci>,
     ) -> ActionExecution<Fibonacci> {
-        match ready.await.unwrap() {
-            ActionQueueResult::Execute(execute) => {
-                execute.begin(run_fibonacci_action)
+        match handle.ready().await {
+            ActionQueueResult::Execute(accepted) => {
+                accepted.execute(run_fibonacci_action)
             }
             ActionQueueResult::Cancel(cancellation) => {
                 cancellation.canceled(Fibonacci_Result::default())
